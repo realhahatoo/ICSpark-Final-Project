dff62ed641690e29c78ddb3b862294c44ba1408c
JeremiahS13 <148928125+realhahatoo@users.noreply.github.com>
2025-03-29 22:14:38 +0000

    Save

---
+++ madLibs.js
@@ -0,0 +1,142 @@
+function adventure() {
+    let adjective1 = prompt("Name an adjective");
+    let noun1 = prompt("Name a noun");
+    let object1 = prompt("Name an object");
+    let adjective2 = prompt("Name an adjective");
+    let sound = prompt("Name a sound");
+    let name1 = prompt("Name a funny name");
+    let noun2 = prompt("Name a noun");
+    let name2 = prompt("Name a funny name");
+    let verb = prompt("Name a verb");
+    let creature = prompt("Name a creature");
+    let food = prompt("Name a food");
+    let adjective3 = prompt("Name an adjective");
+    let noun3 = prompt("Name a noun");
+    let verb2 = prompt("Name a verb");
+    let name3 = prompt("Name a funny name");
+    let noun4 = prompt("Name a noun");
+    let adjective4 = prompt("Name an adjective");
+
+    document.getElementById("output").innerHTML = `
+        <p>One sunny morning, a group of defective adventurers set off on a journey into the wild, untamed Amazon jungle. They packed ${adjective1}, ${noun1}, and a giant ${object1}, just in case they encountered a ${adjective2} problem. As they walked through the dense jungle, they suddenly heard a loud ${sound}. "What was that?!" yelled ${name1}, looking around nervously. Out of the trees, a massive ${creature} ${noun2} appeared, covered in ${adjective3}. "This is it!" screamed ${name2}, "We’re gonna get ${verb}!" But just then, a ${adjective4} ${noun3} swooped down and stole their last bag of ${food}! "No! Not the ${noun4}!" cried the group. "We’ll never survive the jungle without it!" They quickly followed the ${creature} into the jungle, which led them to a ${adjective4} waterfall. Beneath the water, they saw something shiny, a ${noun3} treasure chest! "It’s the legendary ${noun4} treasure!" shouted ${name3}. "Let’s open it!" But as they touched the chest, the ground began to ${verb2}, and a giant ${noun4} ${noun3} burst from the earth! "RUN!" yelled ${name1}. The group sprinted through the jungle, dodging ${adjective2} ${noun2} and ${noun3}, until they reached the safety of a ${adjective4} treehouse. Panting and out of breath, ${name3} wiped their brow and sighed, "Well, at least we still have our ${noun1}... and our ${adjective4} sense of adventure."</p>
+    `;
+}
+
+function groceryAdventure() {
+    let holiday = prompt("Enter a holiday");
+    let personName1 = prompt("Enter a person's name");
+    let verb1 = prompt("Enter a verb");
+    let storeName = prompt("Enter a store name");
+    let noun1 = prompt("Enter a noun");
+    let adjective1 = prompt("Enter an adjective");
+    let noun2 = prompt("Enter a noun");
+    let adjective2 = prompt("Enter an adjective");
+    let noun3 = prompt("Enter a noun");
+    let adjective3 = prompt("Enter an adjective");
+    let noun4 = prompt("Enter a noun");
+    let personName2 = prompt("Enter a person's name");
+    let verb2 = prompt("Enter a verb");
+    let verb3 = prompt("Enter a verb");
+    let adjective4 = prompt("Enter an adjective");
+    let typeOfStore = prompt("Enter type of store");
+
+    document.getElementById("output").innerHTML = `
+        <p>It was almost ${holiday}, so ${personName1} knew they had to get grocery shopping. They ${verb1} to the ${storeName} and picked up a ${noun1}. They looked at their list. They needed to get ${adjective1} ${noun2}, ${adjective2} ${noun3}, and ${adjective3} ${noun4}. They couldn't find anything! Eventually, they saw their friend, ${personName2}, and ${verb2} them for help. Their friend ${verb3}. "You're in the ${adjective4} place!" they said. "This is a ${typeOfStore}!"</p>
+    `;
+}
+
+function kitchenAdventure() {
+    let adjective1 = prompt("Enter an adjective");
+    let adjective2 = prompt("Enter an adjective");
+    let adjective3 = prompt("Enter an adjective");
+    let food1 = prompt("Enter food");
+    let food2 = prompt("Enter food");
+    let color = prompt("Enter color");
+    let adjective4 = prompt("Enter an adjective");
+    let bodyPart1 = prompt("Enter a body part");
+    let noun1 = prompt("Enter a noun");
+    let sound = prompt("Enter a sound");
+    let adjective5 = prompt("Enter an adjective");
+    let animal = prompt("Enter an animal");
+    let adjective6 = prompt("Enter an adjective");
+    let adjective7 = prompt("Enter an adjective");
+    let adjective8 = prompt("Enter an adjective");
+    let bodyPart2 = prompt("Enter a body part");
+    let bodyPart3 = prompt("Enter a body part");
+    let verb = prompt("Enter a verb");
+
+    document.getElementById("output").innerHTML = `
+        <p>One ${adjective1} day, Banky was walking through the ${adjective2} kitchen, humming a ${adjective3} tune. He was on his way to make his favorite snack: a ${food1} sandwich with a side of ${food2}. But as he walked, he didn’t notice the ${color} banana peel lying on the ${noun1} floor. Suddenly, Banky’s ${bodyPart1} hit the peel, and he went flying through the air like a ${animal}! With a ${sound}, he landed right into a giant tub of ${adjective4} maple syrup that was sitting nearby. As Banky tried to get out, he found himself stuck like a ${adjective5} in a ${noun1} trap. The more he struggled, the more ${adjective6} and ${adjective7} he became! Auggie, Livi, and Billy ran in to see what all the commotion was about. When they saw Banky covered in syrup from ${bodyPart2} to ${bodyPart3}, they couldn’t stop ${verb}ing with laughter.</p>
+    `;
+}
+
+function lostRingAdventure() {
+    let adjective1 = prompt("Enter an adjective");
+    let adjective2 = prompt("Enter an adjective");
+    let adjective3 = prompt("Enter an adjective");
+    let adjective4 = prompt("Enter an adjective");
+    let adjective5 = prompt("Enter an adjective");
+    let verbPastTense1 = prompt("Enter a verb (past tense)");
+    let adjective6 = prompt("Enter an adjective");
+    let noun1 = prompt("Enter a noun");
+    let adjective7 = prompt("Enter an adjective");
+    let adjective8 = prompt("Enter an adjective");
+    let adjective9 = prompt("Enter an adjective");
+    let verb1 = prompt("Enter a verb");
+    let adjective10 = prompt("Enter an adjective");
+    let emotion = prompt("Enter an emotion");
+    let adverb = prompt("Enter an adverb");
+    let adjective11 = prompt("Enter an adjective");
+    let noun2 = prompt("Enter a noun");
+    let pluralNoun1 = prompt("Enter plural noun");
+    let adjective12 = prompt("Enter an adjective");
+    let noun3 = prompt("Enter a noun");
+    let adjective13 = prompt("Enter an adjective");
+    let adjective14 = prompt("Enter an adjective");
+    let adjective15 = prompt("Enter an adjective");
+    let adjective16 = prompt("Enter an adjective");
+    let adjective17 = prompt("Enter an adjective");
+    let adjective18 = prompt("Enter an adjective");
+    let noun4 = prompt("Enter a noun");
+    let adjective19 = prompt("Enter an adjective");
+    let adjective20 = prompt("Enter an adjective");
+    let noun5 = prompt("Enter a noun");
+    let termOfEndearment = prompt("Enter a term of endearment");
+    let adjective21 = prompt("Enter an adjective");
+    let noun6 = prompt("Enter a noun");
+    let adjective22 = prompt("Enter an adjective");
+    let verbPastTense2 = prompt("Enter a verb (past tense)");
+
+    document.getElementById("output").innerHTML = `
+        <p>Rachel’s heart did a ${adjective1} sank in the lobby of the Springhill Suites. Her ${adjective2} engagement ring, adorned with a ${adjective3} diamond, was gone! Vanished! In Orlando, of all places, during a ${adjective4} business trip. She’d arrived that afternoon, a blur of ${adjective5} airports and ${noun1} taxis. “It has to be here,” she ${verbPastTense1}, dumping her ${adjective6} suitcase onto the ${noun2}. A ${adjective7} knock announced her coworkers, Jana and Todd. They’d seen her ${adjective8} face in the hallway and came to ${verb1}. “Everything okay?” Todd asked, looking ${adjective9}. “My Ring!” Rachel cried, waving her hands. “I lost it!” My fiancé Karl is going to be so ${emotion}!” Todd and Jana jumped into action. They ${verbPastTense2} searched the ${adjective10} room, under the ${noun3}, behind the ${adjective11}, even in the ${pluralNoun1} ${noun4}. Their ${adjective12} search took them across Orlando. Jana checked ${adjective13} lost and founds, peered into ${adjective14} fountains, and Todd even asked a ${adjective15} Mickey Mouse if they’d seen a ${adjective16} ring. As the sun set, their ${adjective17} hope faded. Rachel felt ${adjective18}. Back at the ${adjective19} hotel, her ${noun5} rang. It was Karl. “Hey, ${termOfEndearment},” he said. “Your ring’s on the ${adjective20} bathroom ${noun6}. You must have left it before you left for the trip.” ${verbPastTense2} silence. Then, laughter. Todd and Jana ${verbPastTense2} with relief.</p>
+    `;
+}
+
+function weddingStory() {
+    let adjective1 = prompt("Enter an adjective");
+    let adjective2 = prompt("Enter an adjective");
+    let color1 = prompt("Enter a color");
+    let color2 = prompt("Enter a color");
+    let noun1 = prompt("Enter a noun");
+    let emotion = prompt("Enter an emotion");
+    let noun2 = prompt("Enter a noun");
+    let noun3 = prompt("Enter a noun");
+    let adjective3 = prompt("Enter an adjective");
+    let verb1 = prompt("Enter a verb");
+    let adjective4 = prompt("Enter an adjective");
+    let noun4 = prompt("Enter a noun");
+    let pluralNoun1 = prompt("Enter a plural noun");
+    let pluralNoun2 = prompt("Enter a plural noun");
+    let shape = prompt("Enter a shape");
+    let adjective5 = prompt("Enter an adjective");
+    let verb2 = prompt("Enter a verb");
+    let adjective6 = prompt("Enter an adjective");
+    let pluralNoun3 = prompt("Enter plural noun");
+    let pluralNoun4 = prompt("Enter plural noun");
+    let adjective7 = prompt("Enter an adjective");
+    let pluralNoun5 = prompt("Enter plural noun");
+
+    document.getElementById("output").innerHTML = `
+        <p>One sunny ${adjective1} afternoon, the big day finally arrived! The ${adjective2} couple, dressed in ${color1} and ${color2}, stood at the end of the ${noun1} as their family and friends cheered. The ceremony was filled with ${emotion}, as the officiant declared, “I now pronounce you ${noun2} and ${noun3}!” The couple sealed the deal with a ${adjective3} kiss that made everyone ${verb1} in delight. At the reception, the ${adjective4} cake stole the show. It was shaped like a ${shape} and topped with ${pluralNoun1}. The guests were so impressed that they yelled out "${pluralNoun2}!" The highlight of the evening was the first dance. The couple twirled like ${pluralNoun3} on the dance floor. Everyone joined in, forming a ${adjective5} circle around the happy pair. By the end of the night, even the ${adjective6} guests were up and ${verb2}-ing! It was a ${adjective7} day full of ${pluralNoun4}, laughter, and ${pluralNoun5}. As the couple left for their honeymoon, everyone waved their ${pluralNoun1}.</p>
+    `;
+}
